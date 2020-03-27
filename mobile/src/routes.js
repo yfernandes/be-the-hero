af8c2e38b00fe63c653df3201e16e025b40afb48
yfernandes <yagoalmeida@gmail.com>
2020-03-27 19:20:40 -0300

    App, Routes and addons

---
+++ mobile/src/routes.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Incidents from "./pages/Incidents";
+import Detail from "./pages/Detail";
+
+const Stack = createStackNavigator();
+
+function Routes() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="Incidents" component={Incidents} />
+				<Stack.Screen name="Detail" component={Detail} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export default Routes;
